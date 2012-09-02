6ae8dcfa6ddff8361f796daa41575df8a3ad9ca9
Kev Zettler <kevzettler@gmail.com>
2012-09-02 14:40:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bertslist::Application.config.secret_token = '47a34f2c88bf77328621461c58e13b4f2a3713bcdd40770d48710e007bcadf7126d70f58b2c7cdd4f63c6ab071ff543a30e0646dbc1bb8d57d673eeb713b9107'
