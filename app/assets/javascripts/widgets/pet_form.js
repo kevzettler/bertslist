6b6bc09fa7e4da18e1a05d5cca5d76670f835da6
Kev Zettler <kevzettler@gmail.com>
2013-04-08 21:47:41 -0700

    view logic and stuff

--- app/assets/javascripts/widgets/pet_form.js
+++ app/assets/javascripts/widgets/pet_form.js
@@ -5,28 +5,38 @@ var pet_form = {
 
   _create: function(){
     $('#pet_form_submit').click($.proxy(this.submitButtonClick, this));
+    $("#pet_species_selector").change($.proxy(this.speciesChange, this));
 
     $.cloudfuji.ajaxy_form.prototype._create.call(this);
     $('#fileupload').bind("fileuploadstop", $.proxy(this.fileUploadOnCompleteAll, this));
-  }
+  },
 
-  ,enable: function(){
+  speciesChange:function(event){
+    this.element.find('[class$=_dependent]').hide();
+
+      var $species_input = $(event.target),
+          species = $species_input.find(':selected').text().toLowerCase();
+
+      this.element.find('.'+species+"_dependent").slideDown();
+  },
+
+  enable: function(){
     $('#pet_form_submit').enable();
     $('#fileupload').enable();
-  }
+  },
 
-  ,submitButtonClick: function(event){
+  submitButtonClick: function(event){
     $(event.target).disable();
     this.element.submit();
     return false;
-  }
+  },
 
-  ,fileUploadOnCompleteAll: function(list){
+  fileUploadOnCompleteAll: function(list){
     $.cloudfuji.ajaxy_form.prototype.submitSuccess.call(this, {message: "Pet listed Successfully"}, "success", {responseText:{error:"Pet Listed Successfully"}});
     this.enable();
-  }
-  
-  ,submit: function(){
+  },
+
+  submit: function(){
     if($('.template-upload').length > 0){
       $('#fileupload').disable();
       $('#fileupload').validationEngine('showPrompt', "Please wait while we upload your images", 'pass', "topLeft", true);
@@ -34,17 +44,17 @@ var pet_form = {
     }else{
       $('#fileupload').validationEngine('showPrompt', "Pets can not be listed without images", 'error', "topLeft", true);
     }
-  }
+  },
 
-  ,submitError:function(jqXHR, text, status){
+  submitError:function(jqXHR, text, status){
     $.cloudfuji.ajaxy_form.prototype.submitError.call(this, jqXHR, text, status);
     this.enable();
-  }
+  },
 
-  ,submitSuccess: function(data, textStatus, jqXHR){
+  submitSuccess: function(data, textStatus, jqXHR){
     console.log("omg submit success", data, textStatus, jqXHR);
     var pet_id;
-    
+
     //strange descrepency between development and production probabl servers 
     if(typeof data.id != "undefined"){ pet_id = data.id; }
     if(typeof data.data != "undefined"){ pet_id = data.data.id;}
